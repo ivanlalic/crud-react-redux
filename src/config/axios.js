c748b6791bb345708a7b0b284de75e87443d1efe
ivanlalic <ivanlalic@gmail.com>
2020-11-18 22:59:21 -0300

    json server updated link

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL: 'http://localhost:4000/'    
+    //baseURL: 'http://localhost:4000/'    
+    baseURL: 'https://my-json-server.typicode.com/ivanlalic/crud-react-redux-db'
 
 })
 
